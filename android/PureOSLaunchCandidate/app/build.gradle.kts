3b2acb4b541b1fed71c7ae2af89537c6790ff91b
ryangmsmith-tech <ryangmsmith@gmail.com>
2026-06-19 19:33:04 -0400

    Upgrade app module to PureOS v17.42 AndroidX build

--- android/PureOSLaunchCandidate/app/build.gradle.kts
+++ android/PureOSLaunchCandidate/app/build.gradle.kts
@@ -1,17 +1,18 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.pureos.mobilecore.v1741"
+    namespace = "com.pureos.mobilecore.v1742"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.pureos.mobilecore.v1741"
+        applicationId = "com.pureos.mobilecore.v1742"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1741
-        versionName = "17.41"
+        versionCode = 1742
+        versionName = "17.42"
     }
 
     buildTypes {
@@ -23,6 +24,7 @@ android {
             )
         }
         debug {
+            isDebuggable = true
             applicationIdSuffix = ".debug"
             versionNameSuffix = "-debug"
         }
@@ -32,4 +34,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.12.0")
 }
